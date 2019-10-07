3d3bffdca20539219d337cca9bb2902070926e9d
Jacob Lipton <jacoblipton@156.218.21.198.tigernet.wifi.dyn.clemson.edu>
2019-10-07 12:56:00 -0400

    built out signup module

---
+++ Website/signup.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row header">
+      <div class="app-title">NFL STATS APP</div>
+    </div>
+    <div class="row page-content">
+      <div class="offset-sm-2 col-sm-8 signup-container row">
+        <div class="col-md-12 signup-title">Sign Up</div>
+        <div class="offset-md-2 col-md-4 input-label">Email:</div><div class="col-md-6"> <input class="signup-input" type="email" id="email" /> </div>
+        <div class="offset-md-2 col-md-4 input-label">Password:</div><div class="col-md-6"> <input class="signup-input" type="password" id="password" /> </div>
+        <div class="offset-md-2 col-md-4 input-label">Confirm Password:</div><div class="col-md-6"> <input class="signup-input" type="password" id="confirmPassword" /> </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
