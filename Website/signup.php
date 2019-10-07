cc4d62852bf6adda8bc0d67df637f4165357b159
Jacob Lipton <jacoblipton@156.218.21.198.tigernet.wifi.dyn.clemson.edu>
2019-10-07 13:07:18 -0400

    updated sigup css

--- Website/signup.php
+++ Website/signup.php
@@ -15,9 +15,14 @@
     <div class="row page-content">
       <div class="offset-sm-2 col-sm-8 signup-container row">
         <div class="col-md-12 signup-title">Sign Up</div>
-        <div class="offset-md-2 col-md-4 input-label">Email:</div><div class="col-md-6"> <input class="signup-input" type="email" id="email" /> </div>
-        <div class="offset-md-2 col-md-4 input-label">Password:</div><div class="col-md-6"> <input class="signup-input" type="password" id="password" /> </div>
-        <div class="offset-md-2 col-md-4 input-label">Confirm Password:</div><div class="col-md-6"> <input class="signup-input" type="password" id="confirmPassword" /> </div>
+        <form>
+          <div class="row">
+            <div class="offset-md-2 col-md-4 input-label">Email:</div><div class="col-md-6"> <input class="signup-input" type="email" id="email" /> </div>
+            <div class="offset-md-2 col-md-4 input-label">Password:</div><div class="col-md-6"> <input class="signup-input" type="password" id="password" /> </div>
+            <div class="offset-md-2 col-md-4 input-label">Confirm Password:</div><div class="col-md-6"> <input class="signup-input" type="password" id="confirmPassword" /> </div>
+            <div class="offset-md-5 col-md-2"> <input class="signup-input" type="submit" id="confirmPassword" value="Sign Up" /> </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
