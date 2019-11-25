add4b88b75b473492960644ed5a57f161c3aac12
Jacob Lipton <jacoblipton@235.5.23.172.generic.clemson.edu>
2019-11-25 18:50:56 -0500

    added SQL functionality to record.php

--- Website/record.php
+++ Website/record.php
@@ -9,23 +9,34 @@
   <script type="text/javascript" src="table.js"></script>
 
   <?php
+    //Connection info
+    $servername = "mysql1.cs.clemson.edu";
+    $username = "bpjoye";
+    $password = "cpsc4620";
+    $dbname = "nfl_db";
+
+    //Connect to DB
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
     //Get POST Data
-    $action = $_GET["action"];
-    $tableName = $_GET["table"];
-    $id = $_GET["id"];
+    $action = substr($_GET["action"], 1, strlen($_GET["action"])-2);
+    $tableName = substr($_GET["table"], 1, strlen($_GET["table"])-2);
+    $id = substr($_GET["id"], 1, strlen($_GET["id"])-2);
 
     //Get JSON Table Data
     $strJsonFileContents = file_get_contents("tables/tableData.json");
     $tableData = json_decode($strJsonFileContents, true);
 
     //Matches table name from querystring to json data
-    $tableName = substr($tableName,1,strlen($tableName)-2);
     $tableColumns = null;
     for ($i = 0; $i < count($tableData["tableNames"]); $i++) {
       if ($tableName == $tableData["tableNames"][$i]["name"]) {
         $tableColumns = $tableData["tableNames"][$i]["columns"];
       }
     }
+
+    $sql = 'SELECT * FROM ' . $tableName . ' WHERE ' . $tableColumns[0]["column"] . '="' . $id . '";';
+    $result = mysqli_query($conn, $sql);
   ?>
 </head>
 <body>
@@ -35,11 +46,25 @@
     </div>
     <div class="row page-content">
       <?php
-        for ($i = 0; $i < count($tableColumns); $i++) {
-          echo '<div class="input-label col-md-2">' . $tableColumns[$i]["displayName"] . ':</div>
-                <div class="col-md-4"><input type="text" name="' . $tableColumns[$i]["column"] . '"/></div>';
+        if ($action == 'edit') {
+          $row = $result->fetch_assoc();
         }
+        for ($i = 0; $i < count($tableColumns); $i++) { ?>
+          <div class="input-label col-md-2"><?php echo $tableColumns[$i]["displayName"]; ?>:</div>
+          <div class="col-md-4">
+            <?php
+              if ($action == 'add') {
+                echo '<input type="text" name="' . $tableColumns[$i]["column"] . '"/>';
+              } else if ($action == 'edit') {
+                echo '<input type="text" name="' . $tableColumns[$i]["column"] . '" value="' . $row[$tableColumns[$i]["column"]] . '"/>';
+              }
+            ?>
+          </div>
+        <?php }
+
+        $conn->close();
       ?>
+      <div><input type="button" /></div>
     </div>
   </div>
 </body>
